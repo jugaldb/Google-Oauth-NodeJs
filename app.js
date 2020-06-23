0f3a4240ff862a68bcd839c6d85dc654c3719e9b
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-06-24 00:33:33 +0530

    all code

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+
+
+
+
+const app = express();
+
+
+
+
+const dbURI = ""//your database connection string
+
+
+const passportSetup = require("./config/passport-setup");
+const passport = require("passport");
+
+const authRoutes = require("./routers/auth");
+
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use("/auth", authRoutes);
+
+
+mongoose
+	.connect(dbURI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Database Connected"))
+	.catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieSession]
+// }));
+
+
+
+
+
+//route not found
+app.use((req, res, next) => {
+	const error = new Error("Route not found");
+	error.status = 404;
+	next(error);
+});
+
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+
+
+
